7fc7618472951723f38d900e762e92a4cf998f4e
Alex Aitken <aaitken@alexaitken.net>
2011-06-17 16:52:49 -0600

    EOD 110617

--- scripts/application.js
+++ scripts/application.js
@@ -34,12 +34,21 @@ var NOTESAPP=(function(){
 				note=new modelNote();
 			note.set(attrs);
 			note.save();
+			e.preventDefault();//stop browser submittal
+			e.stopPropagation();//kill default jquery effects
+
+			$('.ui-dialog').dialog('close');//close dialog
+			this.reset();
+
 		},
 		getAttributes:function(){
 			return{
 				title:this.$('form [name=title]').val(),
 				body:this.$('form [name=body]').val()
 			}
+		},
+		reset:function(){
+			this.$('input, textarea').val('');
 		}
 	});
 
